8274700b43742aaf756fe9605f57fac9f9114a40
Jill Gao <jill.h.gao@gmail.com>
2016-12-05 20:57:46 -0500

    obsolete SearchTester, exist solely for visualizing result, remove later

--- src/search/SearchTester.java
+++ src/search/SearchTester.java
@@ -3,22 +3,20 @@ package search;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Calendar;
-
+/* Obsolete */
 public class SearchTester {
+	
 
 	public static void main(String[] args) throws FileNotFoundException {
-		FileFetcher ff = new FileFetcher(2);
-		BarData bd = new BarData(ff);
 		Calendar now = Calendar.getInstance();
-
+		FileFetcher ff = new FileFetcher(now.DAY_OF_WEEK);
+		BarData bd = new BarData(ff);
 		BarFinder bf = new BarFinder(now, bd);
-		ArrayList<Bar> HHResult = bf.find();
-//		System.out.println(HHResult);
-
-		System.out.println(HHResult);
+		ArrayList<Bar> searchResult = bf.find();
+		
 		int count = 0;
 
-		for (Bar bar : bf.find()) {
+		for (Bar bar : searchResult) {
 			System.out.print(bar.name + ' ' + bar.address + ' ' + bar.lat + ' ' + bar.lon + ' ' + bar.startTimeString + ' ' + bar.endTimeString
 					+ ' ' + bar.descriptionString + '\n');
 			count++;
