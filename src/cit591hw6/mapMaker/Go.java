09c58d88792ac59f56abdf632ec2c29cbbdc4486
JiahuiLii <lijiahui@seas.upenn.edu>
2016-12-17 16:43:04 -0500

    java docs add

--- src/cit591hw6/mapMaker/Go.java
+++ src/cit591hw6/mapMaker/Go.java
@@ -67,7 +67,9 @@ public class Go extends Application implements MapComponentInitializedListener{
 	private ImageView imageView;
 	private String yelpRatingImgUrl;
 
-	
+	/**
+	 * Start to initialize the layout and the window
+	 */
 	@Override
 	public void start(Stage Stage) throws Exception {
 		
@@ -101,7 +103,7 @@ public class Go extends Application implements MapComponentInitializedListener{
 	}
 
 	/**
-	 * initialize the map
+	 * initialize the map, set map options, create map and set go button
 	 */
     public void mapInitialized() {
     	LatLong center = new LatLong(39.952903, -75.164106);
@@ -150,10 +152,10 @@ public class Go extends Application implements MapComponentInitializedListener{
 			}
         });
     }
+
     /**
      * This method runs search algorithm upon button press, initialize DataSender
-     * @param 
-     * @throws FileNotFoundException 
+     * @throws FileNotFoundException
      */
     private void getSearchResult() throws FileNotFoundException {
     	Calendar now = Calendar.getInstance();
@@ -168,6 +170,11 @@ public class Go extends Application implements MapComponentInitializedListener{
 //		System.out.println(searchResult.size());
     }
     
+    /**
+     * put marker for each bar after press the go button
+     * each marker has a info window display their name
+     * also display the message about that bar on side vbox
+     */
     private void putMarker() {
     	//Add all marker to the map
         for (int i = 0; i < ds.getAddrLat().size(); i++){
@@ -254,6 +261,11 @@ public class Go extends Application implements MapComponentInitializedListener{
         }
 	}
     
+    /**
+     * create label of image for yelp
+     * @param imageView the view of image
+     * @return labeled image
+     */
     private Label createLabeledImage(ImageView imageView) {
         Label labeledImage = new Label();
         labeledImage.setGraphic(imageView);
